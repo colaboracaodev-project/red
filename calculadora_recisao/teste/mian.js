daa8d901b625dc29daa935766522471cc8fd3c6b
Eduardo Escudero <escudero.ti@gmail.com>
2023-01-07 20:49:04 -0300

    att

---
+++ calculadora_recisao/teste/mian.js
@@ -0,0 +1,10 @@
+// funções auxiliares
+function calculaSaldoSalario(salarioDigitado, diasMes) {
+    let saldoSalario = (salarioDigitado/30) * diasMes; 
+    return saldoSalario; 
+}
+
+function calculaDecimoTerceiroProporcional(salarioDigitado, mesesAno) {
+    let testedecimoTerceiro = (salarioDigitado/12) * mesesAno; 
+    return decimoTerceiro; 
+}
